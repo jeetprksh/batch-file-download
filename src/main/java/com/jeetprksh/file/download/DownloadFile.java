2c264f4d49f5a267fb24bf779420fe268ce861f5
Jeet Prakash <jeet1989a@gmail.com>
2021-04-19 00:22:15 +0530

    Fixed NPE while extracting the response headers.

--- src/main/java/com/jeetprksh/file/download/DownloadFile.java
+++ src/main/java/com/jeetprksh/file/download/DownloadFile.java
@@ -4,7 +4,9 @@ import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.Header;
 
+import java.util.Objects;
 import java.util.logging.Logger;
 
 class DownloadFile {
@@ -23,8 +25,12 @@ class DownloadFile {
     logger.info("Downloading file from " + this.fileUrl);
     HttpGet get = new HttpGet(this.fileUrl);
     CloseableHttpResponse response = (CloseableHttpResponse) client.execute(get);
-    String contentType = response.getHeader("content-type").getValue();
-    String fileExtension = contentType.split("/")[1];
+    String fileExtension = "default";
+    Header header = response.getHeader("content-type");
+    if (!Objects.isNull(header)) {
+      String contentType = header.getValue();
+      fileExtension = contentType.split("/")[1];
+    }
     return new DownloadedFile(fileExtension, response.getEntity().getContent());
   }
 }
